8ae711abdd7a9a9a124db4d26ac6201299710d23
STPSUN <497432519@qq.com>
2016-11-08 16:54:36 +0800

    Sun1

---
+++ src/test/Sun.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Sun
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("sun");
+
+	}
+
+}
